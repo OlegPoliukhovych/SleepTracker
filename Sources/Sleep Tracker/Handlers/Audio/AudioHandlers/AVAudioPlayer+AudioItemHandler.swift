47116e926e59cc985e1fa0f23847793afda992e0
Oleg Poliukhovych <oleg.poliukhovych@gmail.com>
2020-09-10 22:36:43 +0300

    AudioPlayerItem refactoring
    AudioPlayerItem struct removed, used extension of AVAudioPlayer instead with simplified wrrapper implementation of AudioItemHandler protocol

---
+++ Sources/Sleep Tracker/Handlers/Audio/AudioHandlers/AVAudioPlayer+AudioItemHandler.swift
@@ -0,0 +1,29 @@
+//
+//  AudioItemPlayer.swift
+//  Sleep Tracker
+//
+//  Created by Oleg Poliukhovych on 9/10/20.
+//  Copyright © 2020 Oleg Poliukhovych. All rights reserved.
+//
+
+import AVFoundation
+
+extension AVAudioPlayer: AudioItemHandler {
+
+    convenience init(soundUrl: URL, startTime: Date?) throws {
+        try self.init(contentsOf: soundUrl)
+
+        guard let startDate = startTime else {
+            return
+        }
+        play(atTime: deviceCurrentTime + startDate.timeIntervalSinceNow)
+    }
+
+    func run() {
+        play()
+    }
+
+    func finish() {
+        stop()
+    }
+}
